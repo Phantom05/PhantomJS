4550b155c3ad02a9f699c75fef59a447274d9f7e
phantom05 <monster2jy@gmail.com>
2018-05-14 18:23:02 +0900

    first Array prototype, //Delete the target string in Array

--- PhantomJs-0.0.1.js
+++ PhantomJs-0.0.1.js
@@ -1,1 +1,16 @@
 // hello PhantomJs!
+
+/*
+[Array Area]
+*/
+
+//Delete the target string in Array
+
+Array.prototype.delStr = function(p){
+  let idx = this.indexOf(p);
+  if(p.length == this[idx].length){
+    let num = this.splice(idx,1);
+    return num;
+  }
+}
+
