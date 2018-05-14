2295a917be4e8ec3fc21ec30f1535bb554295aeb
phantom05 <monster2jy@gmail.com>
2018-05-15 00:07:49 +0900

    add Array prototype 1.Get the maximum value by printing only numbers in an array 2.Get values by printing only characters in an array 3.Extract values that exceed the parameter values in the array

--- PhantomJs-0.0.1.js
+++ PhantomJs-0.0.1.js
@@ -4,6 +4,7 @@
 [Array Area]
 */
 
+
 // Delete the target string in Array
 
 Array.prototype.delStr = function(p){
@@ -14,7 +15,9 @@ Array.prototype.delStr = function(p){
   }
 }
 
+
 // Sort method auto sorting "1,11,12,2,3 22,33" Troubleshooting.
+
 Array.prototype.arraySort = function(){
   let allSort = function(p,t){
     return p-t;
@@ -22,10 +25,49 @@ Array.prototype.arraySort = function(){
   return this.sort(allSort);
 }
 
+
 // Reverse of arraySort
+
 Array.prototype.reverseArraySort = function(){
   let allSort = function(p,t){
     return t-p;
   }
   return this.sort(allSort);
+}
+
+
+// Get the maximum value by printing only numbers in an array
+
+Array.prototype.maxNumArray =function(){
+  let newArray = [];
+  let str = this.forEach(function(t){
+    (typeof(t)=='number')? newArray.push(t) :false ;
+  })
+  return newArray.reduce((x,y)=>Math.max(x,y));
+}
+
+
+//Get values by printing only characters in an array
+
+Array.prototype.onlyStrArray=function(){
+  let newArray=[];
+  let str =this.forEach(function(t){
+    (typeof(t)=='string')? newArray.push(t):false;
+  })
+  return newArray;
+}
+
+
+// Extract values that exceed the parameter values in the array
+
+Array.prototype.exceedArray=function(a){
+  let fill = this.filter(function(x){
+    if(!(typeof(a)=='number')){
+      console.error('Please enter values only for number types.');
+      return false;
+    }else{
+      return x >a;
+    }
+  })
+  return fill;
 }
